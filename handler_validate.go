82c39996ef543ce592c9cbba8c67e23c930f12fc
George Tsetsegmaa <macdev@macdev-2.local>
2026-02-20 21:20:24 -0800

    Add handler_validate

--- handler_validate.go
+++ handler_validate.go
@@ -5,36 +5,26 @@ import (
 	"encoding/json"
 )
 
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
-		response, err := json.Marshal(payload)
-		if err != nil {
-			return err
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.WriteHeader(code)
-		w.Write(response)
-		return nil
-	}
-
-func respondWithError(w http.ResponseWriter, code int, msg string) error {
-	return respondWithJSON(w, code, map[string]string{"error": msg})
-}
-
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Body string `json:"body"`
 	}
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
 	err := decoder.Decode(&params)
 	if err != nil {
-		respondWithError(w, 500, "Something went wrong")
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
 		return
 	}
 	if len(params.Body) > 140 {
-		respondWithError(w, 400, "Chirp is too long")
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
 		return
 	}
-	respondWithJSON(w, 200, map[string]bool{"valid": true})
+	respondWithJSON(w, http.StatusOK, returnVals{
+		Valid: true,
+	})
 }
