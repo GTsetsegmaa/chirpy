44b45efb2484928efcceb15348e1c988dea904ea
GTsetsegmaa <gtsetsegm@gmail.com>
2026-04-05 18:46:15 -0700

    Fix formatting and add table for refresh tokens

--- sql/schema/002_chirps.sql
+++ sql/schema/002_chirps.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 CREATE TABLE chirps (
-    id UUID PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    body TEXT NOT NULL UNIQUE,
+    id UUID      PRIMARY KEY,
+    created_at   TIMESTAMP NOT NULL,
+    updated_at   TIMESTAMP NOT NULL,
+    body         TEXT NOT NULL UNIQUE,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
